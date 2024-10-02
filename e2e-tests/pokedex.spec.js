86b0f9d05e5bda716fa627b4362f4b6f66a27bff
daysleeper23 <charnel1986@gmail.com>
2024-10-02 21:21:03 +0300

    Ex 11.9
    - Setup Playwright for e2e testing.
    - Config Playwright development server to start the app while tests are executed.
    -

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,10 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+const baseURL = 'http://localhost:8080/'
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto(baseURL)
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
