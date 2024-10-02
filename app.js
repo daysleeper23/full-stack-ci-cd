3673d53ce39076bbad854c8d5806ac59dc5af259
daysleeper23 <charnel1986@gmail.com>
2024-10-02 22:20:55 +0300

    Fix the code to remove the broken in healthcheck endpoint

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 4000
 app.use(express.static('dist'))
 
 app.get('/health-dex', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
